4661e382b0df72f2a8c892b278c725d9ea6fd333
INDRA SOFIULLAH <gamingbtrus@gmail.com>
2025-12-15 17:37:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,10 @@ import numpy as np
 import sympy as sp
 import matplotlib.pyplot as plt
 
-st.set_page_config(
-    page_title="Kalkulator Integral Pias",
-    page_icon="∫",
-    layout="wide"
-)
+st.markdown("""
+    <style>
+    /* Animasi Judul */
+    @keyframes slideIn {
+        from { transform: translateX(-100%); opacity: 0; }
+        to { transform: translateX(0); opacity: 1; }
+    }
